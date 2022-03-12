457798af104c2e9b1869411e1fd53b4ce95e7397
murdex5 <fkadupiige@gmail.com>
2022-03-12 19:43:32 +0100

    commit on 'master'

---
+++ djcrm/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from leads.views import landing_page, LandingPageView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', LandingPageView.as_view(), name='landing-page'),
+    path('leads/', include('leads.urls', namespace="leads")),
+]
---
+++ leads/urls.py
@@ -0,0 +1,17 @@
+from unicodedata import name
+from django.urls import path
+from .views import (
+    leads_list, lead_detail, lead_create, lead_update, lead_delete,
+    LeadListView, LeadDetailView, LeadCreateView, LeadUpdateView, LeadDeleteView
+    )
+
+app_name = "leads"
+
+
+urlpatterns = [
+    path('', LeadListView.as_view(), name='lead-list'),
+    path('<int:pk>/', LeadDetailView.as_view(), name='lead-detail'),
+    path('<int:pk>/update/', LeadUpdateView.as_view(), name='lead-update'),
+    path('<int:pk>/delete/', LeadDeleteView.as_view(), name='lead-delete'),
+    path('create/', LeadCreateView.as_view(), name='lead-create'),
+]
