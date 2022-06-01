260a54f9b4d794a886e8f66f7018dc31d63665da
murdex5 <fkadupiige@gmail.com>
2022-06-01 10:56:48 +0200

    Removed unwanted code

---
+++ Unwanted_code/scripts.py
@@ -0,0 +1,42 @@
+# def lead_update(request, pk):
+#     lead = Lead.objects.get(id=pk)
+#     form = LeadForm()
+#     if request.method == 'POST':
+#         form = LeadForm(request.POST)
+#         if form.is_valid():
+#             first_name = form.cleaned_data['first_name']
+#             last_name = form.cleaned_data['last_name']
+#             age = form.cleaned_data['age']
+#             lead.first_name = first_name
+#             lead.last_name = last_name
+#             lead.age = age
+#             lead.save()
+#             return redirect("/leads")
+#     context = {
+#         'form': form,
+#         "lead": lead,
+#     }
+#     return render(request, "leads/lead_update.html", context)
+
+
+
+# def lead_create(request):
+    # form = LeadForm()
+    # if request.method == 'POST':
+    #     form = LeadForm(request.POST)
+    #     if form.is_valid():
+    #         first_name = form.cleaned_data['first_name']
+    #         last_name = form.cleaned_data['last_name']
+    #         age = form.cleaned_data['age']
+    #         agent = Agent.objects.first()
+    #         Lead.objects.create(
+    #             first_name=first_name,
+    #             last_name=last_name,
+    #             age=age,
+    #             agent=agent
+    #         )
+    #         return redirect("/leads")
+    # context = {
+    #     'form': form
+    # }
+#     return render(request, "leads/lead_create.html", context)
